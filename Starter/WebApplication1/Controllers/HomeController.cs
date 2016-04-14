7cbca0b2103aeba72b96fa52475c66d53f8f8e0b
Thiago Custodio <Thiago Custodio>
2016-04-13 23:25:00 -0300

    starter / solution

---
+++ Starter/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public async Task<ActionResult> Resize(string name)
+        //{
+        //}
+
+        //public async Task<ActionResult> ListMessages()
+        //{
+        //}
+
+        //public async Task<ActionResult> Gallery()
+        //{
+        //}
+
+        //public async Task<ActionResult> Upload()
+        //{
+        //    if(Request.Files.Count > 0)
+        //    {
+        //        using (BlobService blobService = new BlobService())
+        //        {
+        //            HttpPostedFileWrapper file = Request.Files[0] as HttpPostedFileWrapper;
+
+        //            var stream = file.InputStream;
+        //            var fileName = file.FileName;
+
+        //            await blobService.Upload(stream, fileName);
+        //        }
+        //    }
+        //    return RedirectToAction("Gallery");
+        //}
+
+        //public async Task<ActionResult> Template(string emailTemplate)
+        //{
+        //}
+    }
+}
---
+++ Starter/WebApplication1/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $("a.image").each(function (item) {
+        var a = $(this);
+
+        a.click(function () {
+            var ok = window.confirm("Redimencionar?");
+
+            if (ok == false) return;
+            var link = $(this);
+            var name = link.data("name");
+
+            var url = document.location.protocol + "//" + document.location.host + "/Home/Resize";
+
+            $.post(url, { name: name }, function (result) {
+                if (result)
+                {
+                    document.location.reload();
+                }
+            });
+        });
+    })
+});
